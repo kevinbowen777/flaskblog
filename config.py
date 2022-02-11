b6dd907f8e69c29423e5b849b7572f3e7f6089ce
Kevin Bowen <kevin.bowen@gmail.com>
2022-02-10 20:39:10 -0800

    Add logging to file

--- config.py
+++ config.py
@@ -12,4 +12,4 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['kevin.bowen@gmail.com']
+    ADMINS = ['kevinbowen@protonmail.com']
