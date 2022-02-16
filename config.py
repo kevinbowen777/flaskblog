cddadf94631d50e9d1ea64e06697103633c1b691
Kevin Bowen <kevin.bowen@gmail.com>
2022-02-15 20:59:16 -0800

    Supported languages list added

--- config.py
+++ config.py
@@ -13,4 +13,5 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['kevinbowen@protonmail.com']
+    LANGUAGES = ['en', 'es']
     POSTS_PER_PAGE = 10
