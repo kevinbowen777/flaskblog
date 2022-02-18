af29d0526783f928cdb8fa6c253c292ddcfb5943
Kevin Bowen <kevin.bowen@gmail.com>
2022-02-18 11:55:27 -0800

    Add note re: key-gen

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 
 class Config(object):
+    # python3 -c "import uuid; print(uuid.uuid4().hex)"
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'its-a-mystery-to-me'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
