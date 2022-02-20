79db689e21d05456af0644e8982ea30d14d5c0eb
Kevin Bowen <kevin.bowen@gmail.com>
2022-02-20 10:13:45 -0800

    Remove update sub-command

--- app/cli.py
+++ app/cli.py
@@ -9,15 +9,6 @@ def register(app):
         pass
 
     @translate.command()
-    def update():
-        """Update all languages."""
-        if os.system('pybabel extract -F babel.cfg -k _l -o messages.pot .'):
-            raise RuntimeError('extract command failed')
-        if os.system('pybabel update -i messages.pot -d app/translations'):
-            raise RuntimeError('update command failed')
-        os.remove('messages.pot')
-
-    @translate.command()
     def compile():
         """Compile all languages."""
         if os.system('pybabel compile -d app/translations'):
