f5e4b7bb6b9293e71ef849fb74bf68eb257d1e72
Kevin Bowen <kevin.bowen@gmail.com>
2022-02-20 10:12:52 -0800

    Remove init-sub command

--- app/cli.py
+++ app/cli.py
@@ -9,17 +9,6 @@ def register(app):
         pass
 
     @translate.command()
-    @click.argument('lang')
-    def init(lang):
-        """Initialize a new language."""
-        if os.system('pybabel extract -F babel.cfg -k _l -o messages.pot .'):
-            raise RuntimeError('extract command failed')
-        if os.system(
-                'pybabel init -i messages.pot -d app/translations -l ' + lang):
-            raise RuntimeError('init command failed')
-        os.remove('messages.pot')
-
-    @translate.command()
     def update():
         """Update all languages."""
         if os.system('pybabel extract -F babel.cfg -k _l -o messages.pot .'):
