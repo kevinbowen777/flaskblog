9cbdce63be4357eb630cad66ad815fdcb13c1dc5
Kevin Bowen <kevin.bowen@gmail.com>
2022-02-22 15:16:46 -0800

    Populate db with fake users

---
+++ app/fake.py
@@ -0,0 +1,39 @@
+import random
+from random import randint
+from sqlalchemy.exc import IntegrityError
+from faker import Faker
+from app import db
+from .models import User, Post
+
+
+def users(count=100):
+    fake = Faker()
+    i = 0
+    while i < count:
+        u = User(email=fake.email(),
+                 username=fake.user_name(),
+                 password_hash='password',
+                 about_me=fake.text(),
+                 last_seen=fake.past_date())
+        db.session.add(u)
+        try:
+            db.session.commit()
+            i += 1
+        except IntegrityError:
+            db.session.rollback()
+
+
+def posts(count=100):
+    fake = Faker()
+    #user_count = User.query.count()
+    # user_count = 104
+    for i in range(count):
+        guess = 100
+        # u = User.query.offset(randint(0, user_count - 1)).first()
+        u = User.query.offset(guess).first()
+        p = Post(body=fake.text(),
+                 timestamp=fake.past_date(),
+                 user_id=u)
+        db.session.add(p)
+        guess -= 1
+    db.session.commit()
