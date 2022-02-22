d37a2788c8cb69334e69752415011b65c9e8507c
Kevin Bowen <kevin.bowen@gmail.com>
2022-02-22 05:23:47 -0800

    Remove LANGUAGE variable

--- config.py
+++ config.py
@@ -19,6 +19,5 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['kevinbowen@protonmail.com']
-    LANGUAGES = ['en']
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
     POSTS_PER_PAGE = 10
