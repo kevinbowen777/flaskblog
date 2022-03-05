1f60dd0bdcccfedd714ebf3882bc1497697ddf47
Kevin Bowen <kevin.bowen@gmail.com>
2022-03-05 00:49:39 -0800

    Set task progress

--- app/tasks.py
+++ app/tasks.py
@@ -1,19 +1,20 @@
 import time
 from rq import get_current_job
-from app import create_app
+from app import create_app, db
+from app.models import Task
 
-app = create_app
+app = create_app()
 app.app_context().push()
 
 
-def example(seconds):
+def _set_task_progress(progress):
     job = get_current_job()
-    print('Starting task')
-    for i in range(seconds):
-        job.meta['progress'] = 100.0 * i / seconds
+    if job:
+        job.meta['progress'] = progress
         job.save_meta()
-        print(i)
-        time.sleep(1)
-    job.meta['progress'] = 100
-    job.save_meta()
-    print('Task completed')
+        task = Task.query.get(job.get_id())
+        task.user.add_notification('task_progress', {'task_id': job.get_id(),
+                                                     'progress': progress})
+        if progress >= 100:
+            task.complete = True
+        db.session.commit()
