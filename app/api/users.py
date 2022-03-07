a106c26ea20519d973c3245d128496f753ab1a19
Kevin Bowen <kevin.bowen@gmail.com>
2022-03-06 19:19:23 -0800

    Modify/update user

--- app/api/users.py
+++ app/api/users.py
@@ -1,11 +1,14 @@
-from flask import jsonify, request
+from flask import jsonify, request, url_for
+from app import db
 from app.models import User
 from app.api import bp
+from app.api.errors import bad_request
 
 
 @bp.route('/users/<int:id>', methods=['GET'])
 def get_user(id):
     return jsonify(User.query.get_or_404(id).to_dict())
+
 
 @bp.route('/users', methods=['GET'])
 def get_users():
@@ -13,6 +16,7 @@ def get_users():
     per_page = min(request.args.get('per_page', 10, type=int), 100)
     data = User.to_collection_dict(User.query, page, per_page, 'api.get_users')
     return jsonify(data)
+
 
 @bp.route('/users/<int:id>/followers', methods=['GET'])
 def get_followers(id):
@@ -23,6 +27,7 @@ def get_followers(id):
                                    'api.get_followers', id=id)
     return jsonify(data)
 
+
 @bp.route('/users/<int:id>/followed', methods=['GET'])
 def get_followed(id):
     user = User.query.get_or_404(id)
@@ -32,11 +37,38 @@ def get_followed(id):
                                    'api.get_followed', id=id)
     return jsonify(data)
 
+
 @bp.route('/users', methods=['POST'])
 def create_user():
-    pass
+    data = request.get_json() or {}
+    if 'username' not in data or 'email' not in data or 'password' not in data:
+        return bad_request('must include username, email and password fields')
+    if User.query.filter_by(username=data['username']).first():
+        return bad_request('please use a different username')
+    if User.query.filter_by(email=data['email']).first():
+        return bad_request('please use a different email address')
+    user = User()
+    user.from_dict(data, new_user=True)
+    db.session.add(user)
+    db.session.commit()
+    response = jsonify(user.to_dict())
+    response.status_code = 201
+    response.headers['Location'] = url_for('api.get_user', id=user.id)
+    return response
+
 
 @bp.route('/users/<int:id>', methods=['PUT'])
 def update_user(id):
-    pass
+    user = User.query.get_or_404(id)
+    data = request.get_json() or {}
+    if 'username' in data and data['username'] != user.username and \
+            User.query.filter_by(username=data['username']).first():
+        return bad_request('please use a different username')
+    if 'email' in data and data['email'] != user.email and \
+            User.query.filter_by(email=data['email']).first():
+        return bad_request('please use a different email address')
+    user.from_dict(data, new_user=False)
+    db.session.commit()
+    return jsonify(user.to_dict())
+
 
